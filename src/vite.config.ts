31b0ed1d88f400893b904f7fa05efc7d310bd198
er111eic <57517027+er111eic@users.noreply.github.com>
2025-05-17 10:41:01 +0000

    ㄉ

--- src/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/lis-Repository/', // for GitHub Pages
-  plugins: [react()],
-})
